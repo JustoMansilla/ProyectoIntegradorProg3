dc62da6637bfed232f0abe5b09e37c3ad47e9a93
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-03-19 13:59:49 -0300

    Maquetado NO DINAMICO

--- proyecto-integrador-prog3/src/App.js
+++ proyecto-integrador-prog3/src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Sidebar from './componentes/Sidebar'
+import Topbar from './componentes/Topbar'
+import Tarjetas from './componentes/Tarjetas'
+import Titulo from './componentes/Titulo'
+import Tarjetas2 from './componentes/Tarjetas2'
+import Footer from "./componentes/Footer"
 
 function App() {
   return (
     <div className="App">
-      <h1>ESTE ES EL PROYECTO CON REACT</h1>
+
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+              <div className="container-fluid">
+                {/* <!-- Page Heading --> */}
+                <div className="d-sm-flex align-items-center justify-content-between mb-4">
+						      <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+					      </div>
+                  <Tarjetas />
+                  {/* <Tarjetas2 /> */}
+              </div> 
+          </div>
+            <Footer />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ proyecto-integrador-prog3/src/componentes/Tarjetas2.js
@@ -0,0 +1,17 @@
+import Tarjetas2A from './Tarjetas2A'
+import Tarjetas2B from './Tarjetas2B'
+
+
+function Tarjetas2() {
+    return (
+    <div className="row">
+        <Tarjetas2A />
+
+        <Tarjetas2B />
+
+    </div>
+
+    );
+}
+
+export default Tarjetas2;
