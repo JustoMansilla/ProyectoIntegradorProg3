e1befd061ad4d8803d93875af75620f672160a3e
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-03-26 12:29:23 -0300

    .

--- proyecto-integrador-prog3/src/App.js
+++ proyecto-integrador-prog3/src/App.js
@@ -6,6 +6,26 @@ import Titulo from './componentes/Titulo'
 import Tarjetas2 from './componentes/Tarjetas2'
 import Footer from "./componentes/Footer"
 
+
+const arrayTarjetas = [
+	{ titulo: 'Products in Data Base',
+	  subtitulo: '135',
+	  icono: 'fas fa-clipboard-list fa-2x text-gray-300',
+	  color: 'card border-left-primary shadow h-100 py-2'
+	},
+	{ titulo: 'Amount in products',
+	  subtitulo: '$546.456',
+	  icono: 'fas fa-dollar-sign fa-2x text-gray-300',
+	  color: 'card border-left-success shadow h-100 py-2'
+	},
+	{ titulo: 'Users quantity',
+	  subtitulo: '38',
+	  icono: 'fas fa-user-check fa-2x text-gray-300',
+	  color: 'card border-left-warning shadow h-100 py-2'
+	}
+ ]
+
+ 
 function App() {
   return (
     <div className="App">
@@ -20,9 +40,18 @@ function App() {
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
 						      <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
 					      </div>
-                  <Tarjetas />
+                {
+                  arrayTarjetas.map(function (seccionTarjetas, idx) {
+                    return(
+                      <div key={ idx }>
+                           <Tarjetas seccionTarjetas={seccionTarjetas} />
+                      </div>
+                    )
+                  })
+                }
+                 
                   <Tarjetas2 /> 
-                 </div> 
+              </div> 
           </div>
             <Footer />
         </div>
